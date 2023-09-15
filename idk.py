820fe1b63357ef1feb12541dc713547d7f245425
David <93440331+ceodavee@users.noreply.github.com>
2023-09-15 16:06:58 +0200

    idgas

--- idk.py
+++ idk.py
@@ -1,6 +1,8 @@
 import requests
 import time
 
+WEBHOOK_URL = "https://discord.com/api/webhooks/1146166632763359272/MJQKKYn4Mks8jg1i6yxM__yhfc6O2DUjrFbC1Mfa-A7dA6d7bWlvq930Evqz4HxRtY1e"
+
 urls = [
     "http://localhost:3000/spin/eu",
     "http://localhost:3000/spin/fr",
@@ -16,6 +18,17 @@ def spam_url(url, count):
             print(f"Sent request to {url}, Status code: {response.status_code}")
         except requests.ConnectionError:
             print(f"Failed to connect to {url}")
+        time.sleep(0.05)
+
+def send_discord_message(content):
+    data = {
+        "content": content
+    }
+    response = requests.post(WEBHOOK_URL, json=data)
+    if response.status_code == 204:
+        print("Successfully sent message to Discord!")
+    else:
+        print("Failed to send message to Discord.")
 
 start_time = time.time()
 
@@ -24,4 +37,6 @@ for url in urls:
 
 end_time = time.time()
 
-print(f"Total time taken: {end_time - start_time:.2f} seconds")
+message = f"Total time taken: {end_time - start_time:.2f} seconds | Sent {request_count} requests to each URL. | Total requests sent: {request_count * len(urls)}"
+print(message)
+send_discord_message(message)
